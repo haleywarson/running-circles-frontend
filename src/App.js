85f4f81e333b74c4d55fbd2022ba826acc0c659a
Haley Warson <haleywarson@gmail.com>
2021-05-31 12:59:34 -0600

    Incorporate top nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+
+import MenuAppBar from "./Components/MenuAppBar";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <MenuAppBar />
+    </div>
+  );
 }
 
 export default App;
