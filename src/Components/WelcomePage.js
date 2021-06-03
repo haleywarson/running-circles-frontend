f97a969d57537c50a1a70aca7d90010157e01c85
Haley Warson <haleywarson@gmail.com>
2021-06-03 12:28:16 -0600

    Adds icons on welcome page.

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -5,10 +5,30 @@ export default function WelcomePage({ user }) {
     <div className="welcome-page">
       <h2>welcome, {user.username}</h2>
       <div className="welcome-div-section">
-        <div className="welcome-div">Run</div>
-        <div className="welcome-div">Profile</div>
-        <div className="welcome-div">dafs</div>
-        <div className="welcome-div">asdf</div>
+        <div className="welcome-div">
+          <a href="http://localhost:3001/run">
+            <i class="fas fa-running" id="run-icon"></i>
+          </a>
+          run
+        </div>
+        <div className="welcome-div">
+          <a href="http://localhost:3001/run">
+            <i class="far fa-calendar-plus" id="cal-icon"></i>
+          </a>
+          schedule run
+        </div>
+        <div className="welcome-div">
+          <a href="http://localhost:3001/run">
+            <i class="fas fa-circle-notch" id="circle-icon"></i>
+          </a>
+          my circles
+        </div>
+        <div className="welcome-div">
+          <a href="http://localhost:3001/run">
+            <i class="fas fa-user-circle" id="dashboard-icon"></i>
+          </a>
+          dashboard
+        </div>
       </div>
     </div>
   );
